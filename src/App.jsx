e59d47c16cd428a85471e9dcf52d72e15825d7ae
Viktoria <doduchenko29@gmail.com>
2021-08-04 14:08:55 +0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ThemedButton from './ThemedButton';
+import { themes, ThemeContext } from './themes-context';
+
+class App extends React.Component {
+  state = {
+    theme: themes.light,
+  };
+
+  toggleTheme = () => {
+    const newTheme =
+      this.state.theme === themes.dark ? themes.light : themes.dark;
+    this.setState({
+      theme: newTheme,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state.theme}>
+          <ThemedButton onClick={this.toggleTheme}>Dynamic Theme</ThemedButton>
+        </ThemeContext.Provider>
+        <ThemedButton onClick={this.toggleTheme}>Default Theme</ThemedButton>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/ThemedButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ThemeContext } from './themes-context';
+
+class ThemedButton extends React.Component {
+  render() {
+    console.log(this.context);
+    return (
+      <button
+        {...this.props}
+        style={{
+          backgroundColor: this.context.background,
+          color: this.context.fontColor,
+        }}
+        className="btn"
+      ></button>
+    );
+  }
+}
+
+ThemedButton.contextType = ThemeContext;
+
+export default ThemedButton;
